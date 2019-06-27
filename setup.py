43b369879f2ff5c2983f8e0587463a0ee6204375
Daan Knoope <daanknoope@gmail.com>
2019-06-27 17:54:40 +0200

    Add initial code

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name='jkl_serialization',
+      version='0.1',
+      description='(De)Serialization of JKL objects',
+      author='Daan Knoope',
+      author_email='daan@knoope.dev',
+      license='MIT',
+      packages=find_packages(), install_requires=[],
+      python_requires='>3.6'
+)
