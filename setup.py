955d22deb51f0b096bb0bd53d18b29dc51723e90
Daan Knoope <daanknoope@gmail.com>
2019-06-28 08:49:24 +0200

    Fix homepage

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="(De)Serialization of JKL objects",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="hhttps://github.com/daanknoope/jkl-serialization",
+    url="https://github.com/daanknoope/jkl-serialization",
     python_requires='>3.6',
     packages=setuptools.find_packages(),
     classifiers=[
